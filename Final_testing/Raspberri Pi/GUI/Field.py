b9577d4f947873b5b8f1186b46539b7386174ddf
KAMALESH RAVI <kamaleshravi66@gmail.com>
2025-04-04 00:07:52 +0530

    GUI-added
    Field:
      -Delete function added
      -View Function added
      -Add Function updated

--- Final_testing/Raspberri Pi/GUI/Field.py
+++ Final_testing/Raspberri Pi/GUI/Field.py
@@ -1,10 +1,12 @@
+PASSWORD = 'MARS'
+
 import tkinter as tk
 from tkinter import messagebox
 import pickle
 
 
 
-Field_window=None
+Field_add_window=None
 Main_Window = None
 
 # Save the list of objects to a file
@@ -50,59 +52,111 @@ def Load_Field():
 Field_List = Load_Field()
 
 
+#++++++++++++++++++++++Functional GUI+++++++++++++++++++++++++++
 
 
+def Delete_Field(window):
+    global Main_Window , Field_add_window
+    Main_Window = window
 
 
+    Field_add_window = tk.Toplevel(Main_Window,bg=BG)  # Create a new window
+    Field_add_window.geometry("300x200")  # Set dimensions for the subwindow
+    Field_add_window.title(Titel)
 
+    # Add a button to the subwindow
+
+    bottom_menu = tk.Frame(Field_add_window, bg=BG, height=10)
+    bottom_menu.pack(side="bottom", fill="x")
+    tk.Label(Field_add_window, text="Select Field", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+
+    options = [i.Name for i in Field_List]
+    dropdown_var = tk.StringVar()
+    dropdown_var.set(options[0])  # Set default value
+    dropdown_menu = tk.OptionMenu(Field_add_window, dropdown_var, *options)
+    dropdown_menu.pack(pady=5)
+
+
+    tk.Button(Field_add_window, text="Delete Field",font=("Arial", 14), command=lambda:Del(dropdown_var.get())).pack(pady=10, padx=10)
+
+    tk.Button(bottom_menu, text="Exit", font=("Arial", 14), command=lambda: Field_add_window.destroy()).pack(side="left", padx=20)
+  
+
+def Show_Field(window):
+    global Main_Window , Field_add_window
+    Main_Window = window
+
+
+    Field_add_window = tk.Toplevel(Main_Window,bg=BG)  # Create a new window
+    Field_add_window.geometry("300x200")  # Set dimensions for the subwindow
+    Field_add_window.title(Titel)
+
+    # Add a button to the subwindow
+
+    bottom_menu = tk.Frame(Field_add_window, bg=BG, height=10)
+    bottom_menu.pack(side="bottom", fill="x")
+    tk.Label(Field_add_window, text="Select an Crop", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+
+    options = [i.Name for i in Field_List]
+    dropdown_var = tk.StringVar()
+    dropdown_var.set(options[0])  # Set default value
+    dropdown_menu = tk.OptionMenu(Field_add_window, dropdown_var, *options)
+    dropdown_menu.pack(pady=5)
+
+
+    tk.Button(Field_add_window, text="Show Field",font=("Arial", 14), command=lambda:Show(dropdown_var.get())).pack(pady=10, padx=10)
+
+    tk.Button(bottom_menu, text="Exit", font=("Arial", 14), command=lambda: Field_add_window.destroy()).pack(side="left", padx=20)
+  
 
 def Get_Field(window):
 
-    global Main_Window , Field_window
+    global Main_Window , Field_add_window
     Main_Window = window
 
-    Field_window = tk.Toplevel(Main_Window,bg=BG)  # Create a new window
-    Field_window.geometry("300x500")  # Set dimensions for the subwindow
-    Field_window.title(Titel)
+    Field_add_window = tk.Toplevel(Main_Window,bg=BG)  # Create a new window
+    Field_add_window.geometry("300x500")  # Set dimensions for the subwindow
+    Field_add_window.title(Titel)
 
     # Add a button to close the subwindow
 
-    bottom_menu = tk.Frame(Field_window, bg=BG, height=10)
+    bottom_menu = tk.Frame(Field_add_window, bg=BG, height=10)
     bottom_menu.pack(side="bottom", fill="x")
 
-    tk.Label(Field_window, text="Name:", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
-    name_entry = tk.Entry(Field_window, font=("Arial", 14))
+    tk.Label(Field_add_window, text="Name:", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+    name_entry = tk.Entry(Field_add_window, font=("Arial", 14))
     name_entry.pack(pady=5, padx=10)
 
-    tk.Label(Field_window, text="No.of.Rows", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
-    row_entry = tk.Entry(Field_window, font=("Arial", 14))
+    tk.Label(Field_add_window, text="No.of.Rows", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+    row_entry = tk.Entry(Field_add_window, font=("Arial", 14))
     row_entry.pack(pady=5, padx=10)
     
-    tk.Label(Field_window, text="No.of.Column", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
-    cum_entry = tk.Entry(Field_window, font=("Arial", 14))
+    tk.Label(Field_add_window, text="No.of.Column", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+    cum_entry = tk.Entry(Field_add_window, font=("Arial", 14))
     cum_entry.pack(pady=5, padx=10)
 
-    tk.Label(Field_window, text="Select an Crop", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+    tk.Label(Field_add_window, text="Select an Crop", font=("Arial", 12), bg=BG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
 
     options = ["Ground Nut", "Cron", "Wheat"]
     dropdown_var = tk.StringVar()
     dropdown_var.set(options[0])  # Set default value
-    dropdown_menu = tk.OptionMenu(Field_window, dropdown_var, *options)
+    dropdown_menu = tk.OptionMenu(Field_add_window, dropdown_var, *options)
     dropdown_menu.pack(pady=5)
 
 
-    tk.Button(Field_window, text="Add Field",font=("Arial", 14), command=lambda:add(name_entry.get(),row_entry.get(),cum_entry.get(),dropdown_var.get(),Field_window)).pack(pady=10, padx=10)
+    tk.Button(Field_add_window, text="Add Field",font=("Arial", 14), command=lambda:add(name_entry.get(),row_entry.get(),cum_entry.get(),dropdown_var.get(),Field_add_window)).pack(pady=10, padx=10)
+    tk.Button(bottom_menu, text="Exit", font=("Arial", 14), command=lambda: Field_add_window.destroy()).pack(side="left", padx=20)
+  
 
-
-def Field_Close(s,Field_window):
-    Field_window.destroy()
+def Field_Close(s,Field_add_window):
+    Field_add_window.destroy()
     messagebox.showinfo("Info", s)
     
     
 for i in Field_List:
     print(i.Name)    
 
-def add(n,r,c,crop,Field_window):
+def add(n,r,c,crop,Field_add_window):
     try:
 
         obj = Field(n,int(r),int(c),crop)
@@ -111,9 +165,63 @@ def add(n,r,c,crop,Field_window):
         Save_Fields(Field_List)
         
         
-        Field_Close("Field Added",Field_window)
+        Field_Close("Field Added",Field_add_window)
 
 
     except:
         print("Error")
-        Field_Close("Error",Field_window)
+        Field_Close("Error",Field_add_window)
+
+def Del(F):
+    try:
+        
+        for i in Field_List:
+            if i.Name == F:
+                obj = i
+                break
+        n=3
+        while not (n==0):
+            pas = input("Enter Password to delete field: ")
+            if pas == PASSWORD:
+                Field_List.remove(obj)
+                Save_Fields(Field_List)
+                Field_Close("Field Deleted",Field_add_window)
+            n-=1       
+        
+        Field_Close("Invalid Password",Field_add_window)
+
+
+    except:
+        print("Error")
+        Field_Close("Error",Field_add_window)
+
+def Show(F):
+    try:
+        
+        for i in Field_List:
+            if i.Name == F:
+                obj = i
+                break
+
+        global Main_Window , Field_add_window
+
+        sBG="pink"
+        Sub_window = tk.Toplevel(Field_add_window,bg=sBG)  # Create a new window
+        Sub_window.geometry("300x300")  # Set dimensions for the subwindow
+        Sub_window.title("Field Show")
+
+        # Add a button to close the subwindow
+
+        bottom_menu = tk.Frame(Sub_window, bg=sBG, height=10)
+        bottom_menu.pack(side="bottom", fill="x")
+
+        tk.Label(Sub_window, text=f"Name: {obj.Name}", font=("Arial", 12), bg=sBG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+        tk.Label(Sub_window, text=f"Dimention: {obj.Row}x{obj.Com}", font=("Arial", 12), bg=sBG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+        tk.Label(Sub_window, text=f"No.of.Plants: {obj.No_Plants}", font=("Arial", 12), bg=sBG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+        tk.Label(Sub_window, text=f"Crop Planted: {obj.Crop}", font=("Arial", 12), bg=sBG, fg="darkblue").pack(pady=5, padx=10, anchor="w")
+        tk.Button(bottom_menu, text="Exit", font=("Arial", 14), command=lambda: Sub_window.destroy()).pack(side="left", padx=20)
+  
+    except:
+        print("Error")
+        Field_Close("Error",Field_add_window)
+
--- Final_testing/Raspberri Pi/GUI/Main_GUI.py
+++ Final_testing/Raspberri Pi/GUI/Main_GUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
 
-from Field import Get_Field
+from Field import Get_Field, Delete_Field, Show_Field
 
 window = tk.Tk()
 window.title("GUI")
@@ -53,8 +53,87 @@ def show_help():
 def Will_Add():
     print("Yet to develop")
 
-def Filed_window():
+def Field_add_window():
     Get_Field(window)
+
+def Field_Del_window():
+    Delete_Field(window)
+
+def Field_Show_window():
+    Show_Field(window)
+
+
+#++++++++++++++++++++++Drop Down Button++++++++++++++++++++++++++++++++++++++
+
+popup_buttons = []
+
+def clear_previous_buttons():
+    # Destroy any existing buttons in the popup_buttons list
+    global popup_buttons
+    for button in popup_buttons:
+        button.destroy()
+    popup_buttons = []  # Clear the list
+
+def show_buttons_1():
+    clear_previous_buttons()  # Clear the previous buttons
+    
+    # Create and display 4 new buttons to the right of Main Button 2
+    btn1 = tk.Button(window, text="button" , command=Will_Add)
+    btn1.place(x=300, y=180)  # Place right of Main Button 2
+    popup_buttons.append(btn1)
+    
+    btn2 = tk.Button(window, text="button" , command=Will_Add)
+    btn2.place(x=300, y=210)
+    popup_buttons.append(btn2)
+    
+    btn3 = tk.Button(window, text="button" , command=Will_Add)
+    btn3.place(x=300, y=240)
+    popup_buttons.append(btn3)
+    
+    btn4 = tk.Button(window, text="button" , command=Will_Add)
+    btn4.place(x=300, y=270)
+    popup_buttons.append(btn4)
+
+
+def show_buttons_2():
+    clear_previous_buttons()  # Clear the previous buttons
+    
+    # Create and display 4 new buttons to the right of Main Button 2
+    btn1 = tk.Button(window, text="ADD" , command=Field_add_window)
+    btn1.place(x=300, y=180)  # Place right of Main Button 2
+    popup_buttons.append(btn1)
+    
+    btn2 = tk.Button(window, text="VIEW" , command=Field_Show_window)
+    btn2.place(x=300, y=210)
+    popup_buttons.append(btn2)
+    
+    btn3 = tk.Button(window, text="DELETE" , command=Field_Del_window)
+    btn3.place(x=300, y=240)
+    popup_buttons.append(btn3)
+    
+    btn4 = tk.Button(window, text="Edit" , command=Will_Add)
+    btn4.place(x=300, y=270)
+    popup_buttons.append(btn4)
+
+def show_buttons_3():
+    clear_previous_buttons()  # Clear the previous buttons
+    
+    # Create and display 4 new buttons to the right of Main Button 2
+    btn1 = tk.Button(window, text="button" , command=Will_Add)
+    btn1.place(x=300, y=180)  # Place right of Main Button 2
+    popup_buttons.append(btn1)
+    
+    btn2 = tk.Button(window, text="button" , command=Will_Add)
+    btn2.place(x=300, y=210)
+    popup_buttons.append(btn2)
+    
+    btn3 = tk.Button(window, text="button" , command=Will_Add)
+    btn3.place(x=300, y=240)
+    popup_buttons.append(btn3)
+    
+    btn4 = tk.Button(window, text="button" , command=Will_Add)
+    btn4.place(x=300, y=270)
+    popup_buttons.append(btn4)
 
 
 # Left side menu frame
@@ -66,10 +145,10 @@ bottom_menu = tk.Frame(window, bg=BG, height=10)
 bottom_menu.pack(side="bottom", fill="x")
 
 # Add buttons to the side menu (similar to a menu)
-tk.Button(side_menu, text="BITE", font=("Arial", 14), command=Will_Add).pack(pady=2, padx=50)
-tk.Button(side_menu, text="Field", font=("Arial", 14), command=Filed_window).pack(pady=2, padx=50)
-tk.Button(side_menu, text="Task", font=("Arial", 14), command=Will_Add).pack(pady=2, padx=50)
-tk.Button(side_menu, text="Connect", font=("Arial", 14), command=Will_Add).pack(pady=2, padx=50)
+tk.Button(side_menu, text="BITE", font=("Arial", 14), command=show_buttons_1).pack(pady=2, padx=50)
+tk.Button(side_menu, text="Field", font=("Arial", 14), command=show_buttons_2).pack(pady=2, padx=50)
+tk.Button(side_menu, text="Task", font=("Arial", 14), command=show_buttons_3).pack(pady=2, padx=50)
+tk.Button(side_menu, text="Connect", font=("Arial", 14), command=show_buttons_3).pack(pady=2, padx=50)
 #tk.Button(side_menu, text="Exit", font=("Arial", 14), command=window.quit).pack(pady=10, padx=20)
 
 # Add buttons to the bottom menu
