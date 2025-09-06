469c28fe8a23aa476b6ef4ae978448d33faaaef6
KAMALESH RAVI <kamaleshravi66@gmail.com>
2025-09-07 02:43:51 +0530

    just

--- Final_testing/PICO/Sensor Module/dev.py
+++ Final_testing/PICO/Sensor Module/dev.py
@@ -15,8 +15,6 @@ from machine import Pin, UART
 import utime
 
 
-
-
 DATA = [0 for i in range(20)] # Data Regiters
 
 class DEV:
@@ -24,6 +22,7 @@ class DEV:
     SlaveRegFlag = True
     Slave_Value = [[0,0,0,0],[1,0,0,0],[0,1,0,0],[1,1,0,0]] #
     SPin = [] #Slave Pin declearation added @ Rutime
+    FIFO = []
 
     def __init__(self,ID,Name, Slave = False):
         self.ID = int(ID)
@@ -41,20 +40,20 @@ class DEV:
         return (f'Device Name: {self.Name}\nID:{self.ID}')
 
     def Write(self,addr,data): #Fuction for Device's Reg write from Host
-        cmd = str(addr)+"'w'"+str(DEV.Encode(data))
+        cmd = str(addr)+'|w|'+str(DEV.Encode(data))+']['
         self.Send(cmd)
 
     def Read(self,addr): #Fuction for Device's Reg read to Host
-        cmd = str(addr)+"'r"
+        cmd = str(addr)+"|r]["
         return self.Send(cmd,R=True)
         
     def Send(self,data,R=False): #UART Read / Write defintion
         if self.Slave:
             DEV.S_Sel(self.ID -1)
             utime.sleep(0.01)
-            DEV.com.write(data)
+            DEV.com.write(data.encode())
             print(data, "=>sent")
-            utime.sleep(0.1)
+            utime.sleep(0.5)
             if R:
                 utime.sleep(0.25)
                 i=5
@@ -71,38 +70,46 @@ class DEV:
                     i-=1
                 print(f"\nReceived Data: {data}")
                 DEV.S_Sel(0)
+                utime.sleep(0.1)
                 return data 
             else:
                 DEV.S_Sel(0)
         else:
-            DEV.com.write(data)
+            DEV.com.write(data.encode())
             print(f"Data Sent = {data}")
 
 
     def Receive(self): # UART RX Decode
         if DEV.com.any():
-            raw_data=str(DEV.com.read())
-            #raw_data="b'"+input()+"'"
-            print('\nRAW:'+raw_data)
+            raw_data=(DEV.com.read())
+            raw_data=raw_data.decode()
 
-            if 'b"' in raw_data:
-                data=raw_data.split('"')[1].split("'")
+            print('RAW:'+raw_data)
 
-            else:
-                data = raw_data[2:-1].split("'")
+            data = raw_data.split('][')
+            
+            for i in data:
+                if '%' in i or '|' in i:
+                    DEV.FIFO.append(i)
+            del(data)
 
+        for cmd in DEV.FIFO:
 
             if self.Slave:              #Master read raw data
-                return DEV.Decode(data[0])
-            else:                           #Slave decode and response.
+                DEV.FIFO = []
+                return DEV.Decode(cmd)
+            else:
+                data =  cmd.split('|')                         #Slave decode and response.
                 if data[1] == 'r':          #read operation
-                    DEV.com.write(DEV.reg_getdata(int(data[0])))
+                    DEV.com.write(DEV.reg_getdata(int(data[0])).encode())
                     print("Reg Data sent")
                 elif data[1] == 'w':        #write operation
                     DEV.reg_putdata(int(data[0]),data[2])
                 else:
-                    print("MEM R/W error occures")    
-                return True            
+                    print("MEM R/W error occures")
+                
+            DEV.FIFO = []
+            return True            
         return False
     
     def BITE(self): # BITE() 
@@ -141,16 +148,19 @@ class DEV:
             dt = 'd'
         elif type(data) == float:
             dt = 'f'
-        return str(data)+'_'+dt
+        return str(data)+'%'+dt
     
     def Decode (data):          #Distruct the Data from Type
-        t=data.split('_')
+        t=data.split('%')
         dt=t[1]
         if dt == 'd':
             return int(t[0])
         elif dt == 'f':
             return float(t[0])
         return t[0]
+    
+
+
 
 
 
